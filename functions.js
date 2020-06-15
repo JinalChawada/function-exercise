1cca8731de3923df446fd5796eb8b9be46af5704
JinalChawada <jinalchavda2520@gmail.com>
2020-06-15 16:04:53 -0400

    Updated name and date

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+
+/** Jinal Chawada
+ 8th June 2020* 
+ */
 // This function returns a random number between (and including both) minimum and maximum
 function getRandomInteger (minimum, maximum) {
   minimum = Math.ceil(minimum) // minimum is inclusive
